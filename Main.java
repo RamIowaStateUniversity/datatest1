1eff59817cd0c3bb9445badfa2235dfdced33b7c
RamIowaStateUniversity <rramu@iastate.edu>
2016-07-05 16:01:16 -0500

    4

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+enum UserStatus {
+    PENDING,
+    ACTIVE,
+    INACTIVE,
+    DELETED;
+}
+
+public class Main {
+	private void getGeneratedClass(String cl) {
+		int i=0;
+		System.out.println(UserStatus.ACTIVE);
+	}
+}
